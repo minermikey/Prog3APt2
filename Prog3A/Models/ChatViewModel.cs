1d8d26f5b3b7ca86f6c5daed74f862c7e54d7fab
minermikey <michaelsingh12345678910@gmail.com>
2025-05-11 22:03:36 +0200

    Added Shops
    Added The Shop Feature for the Coding

---
+++ Prog3A/Models/ChatViewModel.cs
@@ -0,0 +1,9 @@
+// namespace Prog3A.Models
+// {
+//     public class ChatViewModel
+//     {
+//         public string NewMessage { get; set; }
+//         public List<MessageModel> MessageModel { get; set; }
+//         public string CurrentUserId { get; set; }
+//     }
+// }
