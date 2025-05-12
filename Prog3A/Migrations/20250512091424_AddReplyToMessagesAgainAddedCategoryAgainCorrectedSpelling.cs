2f97b903064e3442509c736ae91b033b2810972f
minermikey <michaelsingh12345678910@gmail.com>
2025-05-12 11:39:01 +0200

    Messages Category
    Added Categry for messages

---
+++ Prog3A/Migrations/20250512091424_AddReplyToMessagesAgainAddedCategoryAgainCorrectedSpelling.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Prog3A.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddReplyToMessagesAgainAddedCategoryAgainCorrectedSpelling : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Category",
+                table: "Messages",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "Messages");
+        }
+    }
+}
