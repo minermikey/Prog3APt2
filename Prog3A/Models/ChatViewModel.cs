83088c1700d8847df9a620a72d8a1d8c59236af0
minermikey <michaelsingh12345678910@gmail.com>
2025-05-14 14:41:25 +0200

    Required Delete

--- Prog3A/Models/ChatViewModel.cs
+++
@@ -1,9 +0,0 @@
-// namespace Prog3A.Models
-// {
-//     public class ChatViewModel
-//     {
-//         public string NewMessage { get; set; }
-//         public List<MessageModel> MessageModel { get; set; }
-//         public string CurrentUserId { get; set; }
-//     }
-// }
--- Prog3A/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Prog3A.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
